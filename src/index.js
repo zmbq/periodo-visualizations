4755089eca0c9a6e447ea35aaad281c6c7449acf
Itay Zandbank <itay@chelem.co.il>
2019-05-05 22:24:14 +0300

    Set up eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-'use strict';
-
 function component() {
     const div = $($.parseHTML('<div></div>'));
     div.html('Hello, webpack and jQuery');
     return div;
 }
 
-$(document).ready(()=> {
+$(document).ready(() => {
     const div = component();
     $('body').append(div);
 });
