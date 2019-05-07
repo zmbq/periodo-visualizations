5b61e179cbe36720083e157fb0275ded247f9cfd
Itay Zandbank <itay@chelem.co.il>
2019-05-07 12:57:17 +0300

    Set up typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+require('./style.scss');
+
+function component() {
+    const div = $("#div")
+    return div;
+}
+
+$(document).ready(() => {
+    const div = component();
+    div.html("Welcome to webpack, Typescript and jQuery");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,52 @@
+/* eslint-disable */
+
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
+  entry: './src/index.ts',
+  output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
     },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }
-      ]
+    {
+      test: /\.tsx?$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/,
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            filename: './index.html',
-            template: './src/index.html',
-        }),
-        new webpack.ProvidePlugin({
-          $: 'jquery',
-        }),
-        new CopyPlugin([
-            { from: './data', to: './data', },
-        ]),
-    ]
-  };
+    {
+      test: /\.scss$/, use: [ 
+        { loader: "style-loader" },
+        { loader: "css-loader" },
+        { loader: "sass-loader" },
+    ] }, 
+    {
+      test: /\.css$/, use: [ 
+        { loader: "style-loader" },
+        { loader: "css-loader" },
+    ] }, 
+  ]},
+  plugins: [
+      new HtmlWebpackPlugin({
+          hash: true,
+          filename: './index.html',
+          template: './src/index.html',
+      }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+      }),
+      new CopyPlugin([
+          { from: './data', to: './data', },
+      ]),
+  ]
+};
