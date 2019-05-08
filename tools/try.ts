44ffbd13ce514630638ef370f02227137473af81
Itay Zandbank <itay@chelem.co.il>
2019-05-08 23:38:11 +0300

    Read data files in try.ts

--- tools/try.ts
+++ tools/try.ts
@@ -1,9 +1,31 @@
+import * as fs from 'fs';
 import { report } from '../src/csv/create-csv';
 
-const full = require('./data/full.json');
-const authority = require('./data/authority.json');
-const bag = require('./data/bag.json');
+class Args {
+    public readonly dataPath: string;
+    constructor() {
+        if (process.argv.length !== 3) {
+            throw new Error('Expected exactly 1 arguments - data path');
+        }
+
+        this.dataPath = process.argv[2];
+    }
+}
+
+function readFile(dataPath, filename) {
+    const file = dataPath + '/' + filename;
+    const buffer = fs.readFileSync(file);
+    const raw = buffer.toString();
+    const obj = JSON.parse(raw);
+
+    return obj;
+}
+
+const args = new Args();
+
+const full = readFile(args.dataPath, 'full.json');
+const authority = readFile(args.dataPath, 'authority.json');
+const bag = readFile(args.dataPath, 'bag.json');
 
 console.log('try is here');
 console.log(bag);
-report();
