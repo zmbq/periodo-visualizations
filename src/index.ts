a9ca714132f59b34eee615b1cb7bce7f5df6101a
shaindel <shaindel.barkats@hotmail.com>
2019-05-12 00:57:48 +0300

    periodo-visua

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,63 @@
 require('bootstrap/dist/css/bootstrap.css')
 require('./style.scss');
 
-function component() {
-    const div = $("#div")
-    return div;
+
+
+$(document).ready(function() {
+    loadJson("../../data/bag.json", "#leftCol",  processData);
+    loadJson("../../data/full.json", "#rightCol",  processData);
+
+});
+function  loadJson(url ,contenair,callBack){
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: "text",
+        success: (data) => callBack(data, contenair)
+    });
+}
+function processData(jsonText,contenair){
+    const jsonObject = JSON.parse(jsonText);
+    const div = $(contenair);
+    writeObject(jsonObject, div);
 }
 
-$(document).ready(() => {
-    const div = component();
-    div.html("Welcome to webpack, Typescript and jQuery");
-});
+function writeObject(obj, parentElement){
+    
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const element = obj[key];
+            var txt = document.createElement("p");
+            var p = document.createElement("label")
+            //p.classList.add('');
+
+            if(typeof element === 'object' && element !== null)
+            {
+                txt.innerHTML = `${key}:`;
+                parentElement.append(txt);
+                //txt.classList.add('coucou');
+                writeObject(element, txt)
+            }
+            else{
+                txt.innerHTML = `${key}:`;
+                p.innerHTML = `${element}`;
+                parentElement.append(txt);
+                //txt.classList.add('coucou');
+                txt.append(p);
+                //p.classList.add('david');
+            }
+        }
+    }
+
+}
+
+// function component() {
+//     const div = $("#div")
+//     return div;
+// }
+
+// $(document).ready(() => {
+//     const div = component();
+//     div.html("Welcome to webpack, Typescript and jQuery");
+// });
