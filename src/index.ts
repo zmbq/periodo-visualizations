6d692b31ac9f9b09d31c8d7f2724a4d8247e0f96
shaindel <shaindel.barkats@hotmail.com>
2019-05-13 15:19:15 +0300

    disabled button + <pre>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
+// import * as fs from 'fs';
+import { CsvCreator } from './csv/create-csv';
 require('bootstrap/dist/css/bootstrap.css')
 require('./style.scss');
 
- $('#submit-button').click(function() {
+$(document).ready(function() {
+    $("#content, #url").bind('keyup', function () {
    
-    var content = $('#content').val();
-    processData(content,"#leftCol")
-    var url = $('#url').val();
-    loadJson(url, "#rightCol",  processData);
+           ($('#content').val() && $('#url').val()) ? $('#submit-button').prop('disabled', false) : $('#submit-button').prop('disabled', true);
+   
+    });
+});
 
+// Disable submit button if URL and Peridos are not full
+// Show error if Periods is not a legal json (JSON.parse fails) or full is not a URL of legal JSON (download or parse fail)
+ $('#submit-button').click(() => {
+   
+    // Read full data from URL
+    // process data (both full and content)
+    // Put CSV (returned from process data) in the CSV block
+    const content = $('#content').val();
+    const  csv = processData(content,"#leftCol");
+    const url = $('#url').val();
+
+    loadJson(url, "#rightCol",  processData);
  });
+
 
 
 function  loadJson(url ,contenair,callBack){
@@ -17,43 +33,52 @@ function  loadJson(url ,contenair,callBack){
         type: 'GET',
         url: url,
         dataType: "text",
-        success: (data) => callBack(data, contenair)
-    });
+        success: (data) => callBack(data, contenair),
+        // error: function () {
+        //     alert();
+        //     alert();
+        //   }
+        });
+   
 }
-function processData(jsonText,contenair){
-    const jsonObject = JSON.parse(jsonText);
-    const div = $(contenair);
-    writeObject(jsonObject, div);
-}
-
-function writeObject(obj, parentElement){
-    
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            const element = obj[key];
-            var txt = document.createElement("p");
-            var p = document.createElement("label")
-            //p.classList.add('');
-
-            if(typeof element === 'object' && element !== null)
-            {
-                txt.innerHTML = `${key}:`;
-                parentElement.append(txt);
-                //txt.classList.add('coucou');
-                writeObject(element, txt)
-            }
-            else{
-                txt.innerHTML = `${key}:`;
-                p.innerHTML = `${element}`;
-                parentElement.append(txt);
-                //txt.classList.add('coucou');
-                txt.append(p);
-                //p.classList.add('david');
-            }
-        }
+function processData(full, data){
+    const creator = new CsvCreator(full);
+    let csv = creator.csvHeader + '\n';
+    for(const row of creator.generateCsvRows(data)) {
+        csv += row + '\n';
     }
 
+    return csv;
 }
+
+// function writeObject(obj, parentElement){
+    
+//     for (const key in obj) {
+//         if (obj.hasOwnProperty(key)) {
+//             const element = obj[key];
+//             var txt = document.createElement("p");
+//             var p = document.createElement("label")
+//             //p.classList.add('');
+
+//             if(typeof element === 'object' && element !== null)
+//             {
+//                 txt.innerHTML = `${key}:`;
+//                 parentElement.append(txt);
+//                 //txt.classList.add('coucou');
+//                 writeObject(element, txt)
+//             }
+//             else{
+//                 txt.innerHTML = `${key}:`;
+//                 p.innerHTML = `${element}`;
+//                 parentElement.append(txt);
+//                 //txt.classList.add('coucou');
+//                 txt.append(p);
+//                 //p.classList.add('david');
+//             }
+//         }
+//     }
+
+// }
 
 // function component() {
 //     const div = $("#div")
--- webpack.config.js
+++ webpack.config.js
@@ -4,49 +4,68 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const tsImportPluginFactory = require('ts-import-plugin')
+
 
 module.exports = {
-  entry: './src/index.ts',
-  output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'bundle.js',
-  },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      }
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
     },
-    {
-      test: /\.tsx?$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/,
+    // externals: ["fs"],
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
     },
-    {
-      test: /\.scss$/, use: [ 
-        { loader: "style-loader" },
-        { loader: "css-loader" },
-        { loader: "sass-loader" },
-    ] }, 
-    {
-      test: /\.css$/, use: [ 
-        { loader: "style-loader" },
-        { loader: "css-loader" },
-    ] }, 
-  ]},
-  plugins: [
-      new HtmlWebpackPlugin({
-          hash: true,
-          filename: './index.html',
-          template: './src/index.html',
-      }),
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-      }),
-      new CopyPlugin([
-          { from: './data', to: './data', },
-      ]),
-  ]
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(tsx|ts)$/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    compilerOptions: {
+                        module: 'es2015'
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" },
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                ]
+            },
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: './index.html',
+            template: './src/index.html',
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+        }),
+        new CopyPlugin([
+            { from: './data', to: './data', },
+        ]),
+    ]
 };
