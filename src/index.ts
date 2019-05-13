8c39d78363efc821a36a93444b579b42ddba0873
Itay Zandbank <itay@chelem.co.il>
2019-05-13 15:46:43 +0300

    Convert to CSV in the browser

--- src/index.ts
+++ src/index.ts
@@ -5,87 +5,50 @@ require('./style.scss');
 
 $(document).ready(function() {
     $("#content, #url").bind('keyup', function () {
-   
            ($('#content').val() && $('#url').val()) ? $('#submit-button').prop('disabled', false) : $('#submit-button').prop('disabled', true);
-   
     });
 });
 
 // Disable submit button if URL and Peridos are not full
 // Show error if Periods is not a legal json (JSON.parse fails) or full is not a URL of legal JSON (download or parse fail)
- $('#submit-button').click(() => {
-   
+ $('#submit-button').click(async () => {
+    debugger;
+    $('error').html('');
+    $('#submit-button').prop('disabled', true);
+
     // Read full data from URL
     // process data (both full and content)
     // Put CSV (returned from process data) in the CSV block
-    const content = $('#content').val();
-    const  csv = processData(content,"#leftCol");
-    const url = $('#url').val();
-
-    loadJson(url, "#rightCol",  processData);
- });
-
-
-
-function  loadJson(url ,contenair,callBack){
-
-    $.ajax({
-        type: 'GET',
-        url: url,
-        dataType: "text",
-        success: (data) => callBack(data, contenair),
-        // error: function () {
-        //     alert();
-        //     alert();
-        //   }
-        });
-   
-}
-function processData(full, data){
-    const creator = new CsvCreator(full);
-    let csv = creator.csvHeader + '\n';
-    for(const row of creator.generateCsvRows(data)) {
-        csv += row + '\n';
+    const periodsText = $('#content').val().toString();
+    let periods: any;
+    try {
+        periods = JSON.parse(periodsText);
+    } catch(e) {
+        $('#error').html("Invalid Periods JSON");
+        return;
     }
 
-    return csv;
-}
+    let full: any;
+    try {
+        full = await $.ajax({
+            type: 'GET',
+            url: $('#url').val().toString(),
+            dataType: "json",
+        });
+    } catch(e) {
+        $('#error').html("Can't load full period data");
+        return;
+    }
 
-// function writeObject(obj, parentElement){
-    
-//     for (const key in obj) {
-//         if (obj.hasOwnProperty(key)) {
-//             const element = obj[key];
-//             var txt = document.createElement("p");
-//             var p = document.createElement("label")
-//             //p.classList.add('');
+    const creator = new CsvCreator(full);
+    const lines: string[] = [];
+    lines.push(creator.csvHeader);
+    for(const row of creator.generateCsvRows(periods)) {
+        lines.push(row);
+    }
 
-//             if(typeof element === 'object' && element !== null)
-//             {
-//                 txt.innerHTML = `${key}:`;
-//                 parentElement.append(txt);
-//                 //txt.classList.add('coucou');
-//                 writeObject(element, txt)
-//             }
-//             else{
-//                 txt.innerHTML = `${key}:`;
-//                 p.innerHTML = `${element}`;
-//                 parentElement.append(txt);
-//                 //txt.classList.add('coucou');
-//                 txt.append(p);
-//                 //p.classList.add('david');
-//             }
-//         }
-//     }
-
-// }
-
-// function component() {
-//     const div = $("#div")
-//     return div;
-// }
-
-// $(document).ready(() => {
-//     const div = component();
-//     div.html("Welcome to webpack, Typescript and jQuery");
-// });
+    const csv = lines.join('\n');
+    $('#csv').html(csv);
+    $('#error').html('');
+    $('#submit-button').prop('disabled', false);
+ });
